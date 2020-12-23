0b874cc407d2a896d2fe9c12e3931bd384cb3e12
sumayah adegbite <alwaqtschool@gmail.com>
2020-12-23 20:32:46 +0100

    change

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
 let menu = document.querySelector(".menu");
-let list = document.querySelector("header nav");
-let section = document.querySelector("section");
-let body = document.querySelector("body")
+let list = document.querySelector(" nav");
+
 
 menu.addEventListener ("click", function(){
-    list.style.display = "block"
-    section.style.marginTop = "90%"
-    body.style.background = "brown"
-    list.style.transition = "all0.5s ease-in-out"
+   list.classList.toggle("shownav")
+  
+
 
 })
  
--- youtube.js
+++ youtube.js
@@ -1,13 +1,11 @@
-let menu = document.querySelector(".menu");
-let list = document.querySelector("header nav");
 
-let body = document.querySelector("body")
+let menu = document.querySelector(".menu");
+let list = document.querySelector(" nav");
 
 menu.addEventListener ("click", function(){
-    list.style.display = "block"
-    
-    body.style.background = "brown"
-    list.style.transition = "all 0.5s ease-in-out"
+   list.classList.toggle("shownav")
 
 })
+ 
+
  
