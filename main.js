9f9c10a79439bd6844034ea588c9d171ad09e330
sumayah adegbite <alwaqtschool@gmail.com>
2020-12-24 06:58:26 +0100

    changed
    change

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 let menu = document.querySelector(".menu");
 let list = document.querySelector(" nav");
-
+let herobanner = document.querySelector(".herobannertext")
 
 menu.addEventListener ("click", function(){
    list.classList.toggle("shownav")
-  
+  herobanner.style.opacity = "0.02"
 
 
 })
